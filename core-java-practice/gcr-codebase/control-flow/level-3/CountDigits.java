3ccc53b873cb13c73eccc20c3db522588358a3c3
Kishan Raghav <kishanraghav1674@gmail.com>
2026-06-10 00:15:24 +0530

    [Kishan Raghav] Added Control Flow Level 3 assignments

--- core-java-practice/gcr-codebase/control-flow/level-3/CountDigits.java
+++ core-java-practice/gcr-codebase/control-flow/level-3/CountDigits.java
@@ -1,1 +1,17 @@
-public class CountDigits { public static void main(String[] args) { } }
+import java.util.*;
+
+public class CountDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        int count = 0;
+
+        while (num != 0) {
+            num = num / 10;
+            count++;
+        }
+
+        System.out.println("Digits = " + count);
+    }
+}
