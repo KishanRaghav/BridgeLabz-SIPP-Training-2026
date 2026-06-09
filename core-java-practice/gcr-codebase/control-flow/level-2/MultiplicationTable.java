442ff16f5396b75cb290f9efa796dd256b18d287
Kishan Raghav <kishanraghav1674@gmail.com>
2026-06-10 00:11:13 +0530

    [Kishan Raghav] Added Control Flow Level 2 assignments

--- core-java-practice/gcr-codebase/control-flow/level-2/MultiplicationTable.java
+++ core-java-practice/gcr-codebase/control-flow/level-2/MultiplicationTable.java
@@ -1,1 +1,13 @@
-public class MultiplicationTable { public static void main(String[] args) { } }
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        for (int i = 6; i <= 9; i++) {
+            System.out.println(num + " * " + i + " = " + (num * i));
+        }
+    }
+}
