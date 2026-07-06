0b1dd929e2daf83560dc07df8496e73c4b0f7762
Kishan Raghav <kishanraghav1674@gmail.com>
2026-07-06 09:29:57 +0530

    [Kishan Raghav] Implement fitness tracker using multiple interfaces and default methods

---
+++ oops-practice/Story-Based/FitnessTracker.java
@@ -0,0 +1,44 @@
+public class FitnessTracker {
+    
+}
+interface Trackable {
+    void logActivity();
+
+    default void resetData() {
+        System.out.println("Fitness data has been reset.");
+    }
+}
+
+interface Reportable {
+    void generateReport();
+}
+
+interface Notifiable {
+    void sendAlert();
+}
+
+class FitnessDevice implements Trackable, Reportable, Notifiable {
+
+    public void logActivity() {
+        System.out.println("Activity logged successfully.");
+    }
+
+    public void generateReport() {
+        System.out.println("Fitness report generated.");
+    }
+
+    public void sendAlert() {
+        System.out.println("Workout reminder sent.");
+    }
+}
+
+public class FitnessTracker {
+    public static void main(String[] args) {
+        FitnessDevice device = new FitnessDevice();
+
+        device.logActivity();
+        device.generateReport();
+        device.sendAlert();
+        device.resetData();
+    }
+}
